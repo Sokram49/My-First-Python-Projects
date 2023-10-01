8d1588a975ae1cbbf2e6d65f89a0c471352d1687
Sokram49 <90869270+Sokram49@users.noreply.github.com>
2023-09-30 22:04:06 -0400

    Quiz game

---
+++ quiz_game.py
@@ -0,0 +1,46 @@
+""" Some quiz where you try to figure out what a word is! """
+
+playing = input("A definiton of a word will be given and you must guess the word, got it? (yes/no)")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Alright let's play :) ")
+score = 0
+
+answer = input("To grasp mentally: ")
+if answer.lower() == "comprehend":
+    print("Good Job")
+    score += 1
+else:
+    print("Wrong it's: Comprehend")
+
+answer = input("To tolerate delay: ")
+if answer.lower() == "patience":
+    print("Good Job")
+    score += 1
+else:
+    print("Wrong it's: Patience")
+
+answer = input("Observation of facts or events: ")
+if answer.lower() == "experience":
+    print("Good Job")
+    score += 1
+else:
+    print("Wrong it's: Experience")
+
+answer = input("The outside of something: ")
+if answer.lower() == "exterior":
+    print("Good Job")
+    score += 1
+else:
+    print("Wrong it's: Exterior")
+
+answer = input("In a way that relates to Tech: ")
+if answer.lower() == "technologically":
+    print("Good Job")
+    score += 1
+else:
+    print("Wrong it's: Technologically")
+
+print(f"You got {score}/5")
